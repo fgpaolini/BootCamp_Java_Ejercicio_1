2cf8bf06dc058fc499831bfda289db7ff5c7d69e
Fernando <fgpaolini@gmail.com>
2022-06-14 14:15:32 +0200

    Version_1

---
+++ src/main/Ejercicio_1.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author Fernando
+ */
+public class Ejercicio_1 {
+    
+    
+    public static void main(String[] args) {
+        
+        int edad = 29;
+        long numero = 666555444L;
+        float estatura = 1.7154f;
+        double peso = 79.50;
+        boolean soltero = true;
+        String nombre = "Fernando";
+        
+        System.out.println(
+                        "Nombre: " + nombre + 
+                        "\nEdad: " + edad + 
+                        "\nNúmero de telefono: " + numero + 
+                        "\nEstatura: " + estatura + 
+                        "\nPeso: " + peso + 
+                        "\nSoltero: " + soltero);
+        
+        
+    }
+    
+}
